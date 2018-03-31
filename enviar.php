5265b655d4ee004c0229c36a9908816881d3eed0
mdelpuertox <37922686+mdelpuertox@users.noreply.github.com>
2018-03-31 02:32:51 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,20 @@
+﻿<?php 
+
+$mail='mdelpuertox@hotmail.com'; 
+
+
+$nombre = $_POST['name']; 
+$email = $_POST['email']; 
+$mensaje = $_POST['message']; 
+
+$thank="index4.html"; 
+
+$message = " 
+nombre:".$nombre." 
+email:".$email." 
+mensaje:".$mensaje.""; 
+
+if (mail($mail,"Formulario de Consulta",$message)) 
+Header ("Location: $thank");
+
+?> 
